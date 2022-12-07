9e7330efdd63a153c08b9b693278b067c561124f
Nick Landas <nick.landas@gmail.com>
2022-12-07 10:14:08 -0600

    added react bootsrap elements for navbar but currently in progress

--- new-app/src/App.js
+++ new-app/src/App.js
@@ -5,6 +5,7 @@ import ProductsContainer from './components/ProductsContainer';
 import Home from './components/Home';
 import ProductDetail from './components/ProductDetail';
 import ProductAddForm from './components/ProductAddForm';
+import {Navbar,Nav,Container} from 'react-bootstrap'
 
 function App() {
   const [products,setProducts]=useState([])
@@ -23,8 +24,24 @@ function App() {
 
   return (
     <div className="App">
-      <ProductsContainer allProducts={products}/>
+       <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#products>Knick-Knacks</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <br />
+      
+      </Navbar>
+    </>
+  );
       <ProductAddForm products={products} setProducts={setProducts} /> 
+      <ProductsContainer allProducts={products}/>
     </div>
   );
 }
